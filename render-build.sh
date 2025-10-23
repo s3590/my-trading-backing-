02b4837695c7f9b363f9a78fe05e5b4f4a8d1cd4
s3590 <sdalshmery@gmail.com>
2025-10-24 01:03:58 +0300

    Delete render-build.sh

--- render-build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# exit on error
-set -o errexit
-
-# تثبيت تبعيات النظام التي تحتاجها Pillow
-apt-get update && apt-get install -y libjpeg-dev zlib1g-dev
-
-# تثبيت مكتبات بايثون
-pip install -r requirements.txt
